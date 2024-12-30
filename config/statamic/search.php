a61fba897552c950f697671f588f75af38b5a04d
Iurie Malai <iurie.malai@gmail.com>
2024-12-30 23:52:53 +0100

    Added search functionality

--- config/statamic/search.php
+++ config/statamic/search.php
@@ -31,10 +31,10 @@ return [
             'fields' => ['title'],
         ],
 
-        // 'blog' => [
-        //     'driver' => 'local',
-        //     'searchables' => 'collection:blog',
-        // ],
+        'blog' => [
+            'driver' => 'local',
+            'searchables' => 'collection:blog',
+        ],
 
     ],
 
